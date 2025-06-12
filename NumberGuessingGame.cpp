6c8dc0e054cc12e3eb3057d6cf8b57de64b98652
RibelAI <benboujlidar@gmail.com>
2025-06-12 02:04:36 +0100

    Inshallah No more procastination

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
-int main(){
-	float Number;
-	int Found = 0;
-	const float S = 21.5;
-	while(Found == 0){
-		std::cout<<"Enter a Guessing for the Number\n";
-		std::cin>>Number;
+#include<cstdlib>
+#include<ctime>
+
+void Hint(int S,int attempts,float Number){
 		if (Number == S) {
-			std::cout<<"You found it GREAT\n";
-			Found = 1;
+			std::cout<<"You found it in "<<attempts<<"GREAT\n"<<std::endl;
 		}
 		else if (Number > S) std::cout<<"COlDER\n";
 		else if(Number < S) std::cout<<"HOTTER\n";
+}
+
+
+
+int main(){
+	float Number;
+	int Found = 0;
+	int attempts= 0;
+
+	srand(time(0));
+	int S = rand();
+	while(Found == 0){
+		std::cout<<"Enter a Guessing for the Number\n";
+		std::cout<<S;
+		std::cin>>Number;
+		Hint(S,attempts,Number);
+		if(Number == S) Found == 1;
+		attempts++;
 	}
 	return 0;
 }
