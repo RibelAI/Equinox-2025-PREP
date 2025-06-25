8bacbf3de5f81c0e5aed8478e7b45fd2ca056ba1
RibelAI <benboujlidar@gmail.com>
2025-06-25 14:12:00 +0100

    InshaAllah No More Procastinations

--- FileStudent.cpp
+++ FileStudent.cpp
@@ -13,11 +13,86 @@ int main(){
 	};
 	Student S;
 	std::string line;
-	std::vector<Student> Vec;
 	std::string SortChoice;
+	std::vector<Student>Vec;
 	std::string SaveChoice;
 	std::string SearchName;
 	float AboveGrade;
+	class StudentDatabase{
+		private :
+			std::vector<Student> StudentDatabase;
+		public :
+			void addstudentovec(Student& S){
+				StudentDatabase.push_back(S);
+			}
+			
+			void LoadFromFile(std::string& filename , Student& S){
+				std::ifstream file(filename);
+				if(file.is_open()){
+					while(getline(file,S.Name)){
+						file>>S.age>>S.grade;
+						file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+						addstudentovec(S);
+					}
+					
+				}
+				else std::cout<<"Your File Couldn t open"<<std::endl;
+				file.close();
+
+				
+			}
+			void SaveToFile(std::string& filename){
+				std::ofstream file2(filename);
+				if(file2.is_open()){
+					for(auto& element:StudentDatabase){
+						file2<<element.Name<<std::endl<<element.age<<std::endl<<element.grade<<std::endl;
+					}
+					}else{
+						std::cout<<"Error Your File couldn t open"<<std::endl;
+					}
+					file2.close();
+
+			
+			
+			}
+			
+			void SortbyChoice(){
+				std::cout<<"For Sorting Now You Must choose how are you going to sort by Name or Age or Grade"<<std::endl;
+				std::string ChoiceMethod;
+				std::cin>>ChoiceMethod;
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				if(ChoiceMethod=="Name") std::sort(StudentDatabase.begin(),StudentDatabase.end(),[](Student& a ,Student& b){return a.Name<b.Name;});
+				else if(ChoiceMethod == "Age") std::sort(StudentDatabase.begin(),StudentDatabase.end(),[](Student& a,Student& b){return a.age<b.age;});
+				else if(ChoiceMethod == "Grade") std::sort(StudentDatabase.begin(),StudentDatabase.end(),[](Student& a,Student& b){return a.grade<b.grade;});
+
+			}
+			
+			void Filter(){
+				std::string ChoiceFilter;
+				std::cout<<"In Order To Filter The Students You Need a criteria to base your choice on :\n1 : Similar Names\n2 : Age=>18\n3 : Grade=>16"<<std::endl;
+				std::cin>>ChoiceFilter;			
+				if(ChoiceFilter == "SimilarNames") {
+					for(int i = 0;i<StudentDatabase.size()-1;i++){
+						if(StudentDatabase[i].Name==StudentDatabase[i+1].Name) std::cout<<StudentDatabase[i].Name<<std::endl<<StudentDatabase[i].age<<std::endl<<StudentDatabase[i].grade<<std::endl<<"--"<<std::endl;
+					}
+					
+				}else if(ChoiceFilter == "Age"){
+					for(auto& element :StudentDatabase){
+						if(element.age>=18){
+							std::cout<<element.Name<<std::endl<<element.age<<std::endl<<element.grade<<std::endl<<"--"<<std::endl;
+						}
+					}
+				}else if(ChoiceFilter == "Grade"){
+					for(auto& element :StudentDatabase){
+						if(element.grade>=16){
+							std::cout<<element.Name<<std::endl<<element.age<<std::endl<<element.grade<<std::endl<<"--"<<std::endl;
+						}
+					}
+				}
+
+			}
+		
+	};
 	do{
 	std::cout<<"Choose :\n1/Add a new Student\n2/Display all students from the file\n3/Search for a student by name\n4/Display All students above a certain grade\nChoice : "<<std::endl;
 	std::cin>>choice;
